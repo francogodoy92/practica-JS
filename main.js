d93e97fcec67daa1bff02b1c399a561011dc33d8
francogodoy92 <franck92elrojo@gmail.com>
2023-02-11 18:42:14 -0300

    Practica

---
+++ main.js
@@ -0,0 +1,57 @@
+/* let miNombre = 'Franco'
+let miApellido = 'Godoy'
+let miEdad= 30
+let miMascota = 'Sheena'
+let edadMascota = 6 
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota)*/
+
+/*let nombreCompleto = miNombre +' '+miApellido
+console.log(nombreCompleto)
+let textoPresentacion = 'Buenas, soy ' + nombreCompleto + ', tengo ' + miEdad + ' años y vivo en Córdoba con mi compañera felina que se llama ' + miMascota + ', quien este mes está cumpliendo ' + edadMascota + ' años :)'
+console.log(textoPresentacion)
+let sumaEdades = miEdad + edadMascota
+let restaEdades = miEdad - edadMascota
+let productoEdades = miEdad * edadMascota
+let divisionEdades = miEdad / edadMascota
+console.log(sumaEdades, restaEdades, productoEdades, divisionEdades)
+ */
+/* let miNombre = prompt('Ingrese su nombre: ')
+let miApellido = prompt('Ingrese su apellido: ')
+let miEdad = Number(prompt('Ingrese su edad: '))
+let miMascota = prompt('Ingrese el nombre de su mascota: ')
+let edadMascota= Number(prompt('Ingrese la edad de su mascota: '))
+let nombreCompleto = miNombre +' '+miApellido
+let textoPresentacion = 'Buenas, soy ' + nombreCompleto + ', tengo ' + miEdad + ' años y vivo en Córdoba con mi compañera felina que se llama ' + miMascota + ', quien este mes está cumpliendo ' + edadMascota + ' años :)'
+let sumaEdades = FrancmiEdad + edadMascota
+let restaEdades = miEdad - edadMascota
+let productoEdades = miEdad * edadMascota
+let divisionEdades = miEdad / edadMascota
+console.log(textoPresentacion, sumaEdades, restaEdades, productoEdades, divisionEdades)
+ */
+/*console.log(miNombre, miApellido, miEdad, miMascota, edadMascota) */
+/* let alumno = {
+    nombre: 'Franco',
+    apellido: 'Godoy',
+    edad: 30,
+    provincia: 'Córdoba',
+    hobby: 'Gaming',
+}
+let mascota = {
+    nombre: 'Sheena',
+    especie: 'Felina',
+    edad: 6,
+    provincia: 'Córdoba',
+    hobby: 'Cajas',
+}
+console.log(mascota.hobby)
+console.log(mascota) */
+let frutas = ['Pera', 'Limón', 'Pomelo', 'Kiwi', 'Sandía']
+console.log(frutas)
+console.log(frutas[4])
+let números = [7, 2, 13, 22, 28]
+console.log(números)
+console.log(números[2])
+let familia = ['hija', 'hermana', 'madre', 'padre', 'abuela']
+console.log(familia[1])
+let textoAleatorio = frutas[1] + ' ' + números[3] + ' ' + familia[4]
+console.log(textoAleatorio)
